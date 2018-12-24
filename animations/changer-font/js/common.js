f034081f273c201b764fa23ff6bcc42a703c7337
Web Revolution <41614367+web-revolution@users.noreply.github.com>
2018-12-24 16:59:33 +1000

    new futures

---
+++ animations/changer-font/js/common.js
@@ -0,0 +1,18 @@
+
+	var fonts = [
+		"Calibri",
+		"Arial",
+		"Papyrus",
+		"Times New Roman",
+		"Tahoma",
+		"Wingdings",
+		"Centaur",
+		"Viner Hand ITC"
+	]
+	var i = 0;
+	setInterval(function() {
+		if (i > fonts.length)
+			i = 0
+		document.querySelector(".glitch").style.fontFamily = fonts[i]
+		i++;
+	}, 250)
