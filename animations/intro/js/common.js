9e9479805dd7566f9a6863134ab26321d8772a0f
Web Revolution <41614367+web-revolution@users.noreply.github.com>
2019-07-09 22:29:11 +1000

    Update

--- animations/intro/js/common.js
+++
@@ -1,7 +0,0 @@
-setTimeout (function () {
-	document.querySelector(".btn").classList.add("active");
-}, 2500);
-
-document.querySelector(".btn").onclick = function () {
-	document.querySelector(".wrapper").classList.add("close");
-}
